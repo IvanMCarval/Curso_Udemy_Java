c86c27e70c00ff0b00514682e39b057fb8fe0346
IVAN MASTRANGE <ivanmcarval@hotmail.com>
2021-04-27 11:22:36 -0300

    segundo exemplo de orientação a objeto

---
+++ Orientação__Objeto/Exemplo2/Course/src/App.java
@@ -0,0 +1,34 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+        System.out.println("Enter product data: ");
+        System.out.print("Name: ");
+        product.name = sc.nextLine();
+        System.out.print("Price: ");
+        product.price = sc.nextDouble();
+        System.out.print("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println("Product data: " + product);
+
+        System.out.println("Enter the number of products to be added in stock: ");
+        int quantity = sc.nextInt();
+        product.addProduct(quantity);
+        System.out.println("Updated data: " + product);
+
+        System.out.println("Enter the number of products to be removed in stock: ");
+        quantity = sc.nextInt();
+        product.removeProduct(quantity);
+        System.out.println("Updated data: " + product);
+
+        sc.close();
+    }
+}
