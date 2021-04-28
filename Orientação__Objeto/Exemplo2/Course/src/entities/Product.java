d3d0887bfe13e52e5364c1ef236854d267c29253
IVAN MASTRANGE <ivanmcarval@hotmail.com>
2021-04-28 16:09:33 -0300

    adicionado construtor

--- Orientação__Objeto/Exemplo2/Course/src/entities/Product.java
+++ Orientação__Objeto/Exemplo2/Course/src/entities/Product.java
@@ -5,6 +5,12 @@ public class Product {
     public Double price;
     public int quantity;
 
+    public Product(String name, Double price, int quantity){
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+
     public double totalValueStock(){
         return price * quantity;
     }
